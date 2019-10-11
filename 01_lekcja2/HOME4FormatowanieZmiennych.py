0448cd495ed3b670b6b41dc2a7dc89a1901c26af
FlyingMedusa <sleboda.m98@gmail.com>
2019-10-11 22:37:23 +0200

    updating homework

---
+++ 01_lekcja2/HOME4FormatowanieZmiennych.py
@@ -0,0 +1,4 @@
+liczby = input("Podaj dowolną wartość liczbową:")
+tekst = input("Podaj dowolną wartość tekstową: ")
+zdanie = "Twoja wartość liczbowa to: {0}, a wartość tekstowa to: {1}"
+print(zdanie.format(liczby, tekst))
