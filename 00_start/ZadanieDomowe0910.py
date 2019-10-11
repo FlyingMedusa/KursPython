bffbe733e8d2ce9fb181729f3a6621e4bef36c4a
FlyingMedusa <sleboda.m98@gmail.com>
2019-10-11 21:04:04 +0200

    change

--- 00_start/ZadanieDomowe0910.py
+++
@@ -1,3 +0,0 @@
-s1 = input("Podaj pierwsze zdanie: \n")
-s2 = input("Podaj drugie zdanie: \n")
-s3 = 
--- 00_start/zmienne.py
+++
@@ -1,9 +0,0 @@
-(dystans, spalanie, cena) = (120, 0.064, 5.04)
-koszt = dystans * spalanie * cena
-print(koszt)
-print("--------------------------------")
-dystans = float(input("Podaj dystans: \n"))
-spalanie = float(input("Podaj spalanie na 100km: \n"))
-cena = float(input("Podaj cenę za litr: \n"))
-koszt = dystans * (spalanie/100) * cena
-print("Wydasz:", koszt)
