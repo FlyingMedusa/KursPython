e1df5ca982e77dc2643c331fd02a1f0fcf4c381f
FlyingMedusa <sleboda.m98@gmail.com>
2019-10-15 22:58:03 +0200

    minor update

---
+++ 02_lekcja3InstrukcjeWarunkowe/UzupelniajaceWhileZad1Symulator3latka.py
@@ -0,0 +1,12 @@
+print("\tWitaj w 'Symulatorze trzylatka' \n")
+print("Ten program symuluje rozmowę z trzyletnnim dzieckiem.")
+print("Spróbuj przerwać to szaleństwo. \n")
+
+response = ""
+
+while response != "Dlatego.":
+    response = input("Dlaczego? \n")
+
+print("Aha. Już wiem.")
+
+input("\n Aby zakończyć program, naciśnij ENTER")
