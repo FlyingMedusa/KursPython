17b1ba4e6ba827ea203ed6ac8973d3ad0f249c03
FlyingMedusa <sleboda.m98@gmail.com>
2019-12-02 18:32:29 +0100

    class update

--- FINAL_Md_To_Html/moduleInside.py
+++ FINAL_Md_To_Html/moduleInside.py
@@ -4,25 +4,52 @@ def inside_checker(line):
 
     line = is_bold1(line)
     line = is_bold2(line)
+    line = is_italic1(line)
     return line
 
 
 def is_bold1(line):
     occ = line.count('**')
     if occ > 1:
-        line = into_bold1(line)
+        if occ % 2 == 0:
+            for i in range(0, occ + 1):
+                if i % 2 == 0:
+                    line = line.replace("**", "<b>", 1)
+                else:
+                    line = line.replace("**", "</b>", 1)
+            return line
+        else:
+            for i in range(0, occ - 1):
+                if i % 2 == 0:
+                    line = line.replace("**", "<b>", 1)
+                else:
+                    line = line.replace("**", "</b>", 1)
+            return line
     return line
 
 
 def is_bold2(line):
     occ = line.count('__')
     if occ > 1:
-        line = into_bold2(line)
+        if occ % 2 == 0:
+            for i in range(0, occ + 1):
+                if i % 2 == 0:
+                    line = line.replace("__", "<b>", 1)
+                else:
+                    line = line.replace("__", "</b>", 1)
+            return line
+        else:
+            for i in range(0, occ - 1):
+                if i % 2 == 0:
+                    line = line.replace("__", "<b>", 1)
+                else:
+                    line = line.replace("__", "</b>", 1)
+            return line
     return line
 
 
-def into_bold1(line):
-    occ = line.count('**')
+def is_italic1(line):
+    occ = line.count('*')
     if occ > 1:
         if occ % 2 == 0:
             for i in range(0, occ + 1):
@@ -38,25 +65,7 @@ def into_bold1(line):
                 else:
                     line = line.replace("**", "</b>", 1)
             return line
-
-
-def into_bold2(line):
-    occ = line.count('__')
-    if occ > 1:
-        if occ % 2 == 0:
-            for i in range(0, occ + 1):
-                if i % 2 == 0:
-                    line = line.replace("__", "<b>", 1)
-                else:
-                    line = line.replace("__", "</b>", 1)
-            return line
-        else:
-            for i in range(0, occ - 1):
-                if i % 2 == 0:
-                    line = line.replace("__", "<b>", 1)
-                else:
-                    line = line.replace("__", "</b>", 1)
-            return line
+    return line
 
 
 def clean(line):
